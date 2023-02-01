377391d3e0186527d3d138bc6888d3e36e04f8d3
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-01 18:49:38 +0330

    Initial commit

---
+++ hello-world/Src/index.ts
@@ -0,0 +1,9 @@
+console.log("First Type Script App");
+
+var ageMSoheil: number = 22;
+let age: number = 123;
+
+if (ageMSoheil === 22)
+    age += 22;
+
+
---
+++ hello-world/dist/Src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("First Type Script App");
+var ageMSoheil = 22;
+let age = 123;
+if (ageMSoheil === 22)
+    age += 22;
+//# sourceMappingURL=index.js.map
